1f821471f093aea52669bc6df96b1798c437996e
Raul Sanchez R. <raul.sr92@gmail.com>
2026-03-20 15:01:30 -0500

    Clase 29 - Creación y Organización de componentes en Angular

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
+/*Importar páginas*/
+import { List } from './domains/products/pages/list/list';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',  //página inicial
+    component: List
+  }
+
+];
