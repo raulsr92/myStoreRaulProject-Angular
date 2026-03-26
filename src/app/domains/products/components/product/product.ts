25ab83cf6c76470de9a7aaa8834284004d550241
Raul Sanchez R. <raul.sr92@gmail.com>
2026-03-26 17:17:18 -0500

    Clase 33: outputs en Angular

--- src/app/domains/products/components/product/product.ts
+++ src/app/domains/products/components/product/product.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
 
 @Component({
   selector: 'app-product',
@@ -13,5 +13,15 @@ export class Product {
   @Input({required:true}) price: number = 0;
 
   @Input({required:true}) title: string = ' ';
+
+  @Output() addToCart = new EventEmitter<string>();  // Forma de declarar un output
+
+  addToCartHandler(){
+    console.log('Click from child')
+
+    //Enviar comunicación al padre
+
+    this.addToCart.emit(`Hola, este es un mesaje desde el hijo, soy el ${this.title}`) //Aqui se emite pero no se escucha auns
+  }
 
 }
--- src/app/domains/products/pages/list/list.ts
+++ src/app/domains/products/pages/list/list.ts
@@ -12,4 +12,11 @@ export class List {
   aleatorio = Math.floor(Math.random()*200)
 
   img = `https://picsum.photos/id/${this.aleatorio}/640/640`
+
+  unpackingMessageFromChild(event: string){
+
+    console.log('Estamos en el padre')
+    console.log(event)  //aqui recien escucha el mensaje -> lo desempaca y escucha lo de la linea 24 en product.ts
+  }
+
 }
