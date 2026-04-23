f5064e56f97f75a767830b565b65ab0e1d37d572
Raul Sanchez R. <raul.sr92@gmail.com>
2026-04-22 20:34:49 -0500

    Clase 44: Interfaz grafica de carrito con signals, evento click y activacion de clases de forma dinamica

--- src/app/domains/shared/components/header/header.ts
+++ src/app/domains/shared/components/header/header.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
 
 @Component({
   selector: 'app-header',
@@ -7,5 +7,15 @@ import { Component } from '@angular/core';
   styleUrl: './header.css',
 })
 export class Header {
+
+  hideSideMenu = signal(true)
+
+  toogleSideMenu(){
+
+    console.log("click en carrito")
+
+    this.hideSideMenu.update(prevState => !prevState)
+
+  }
 
 }
