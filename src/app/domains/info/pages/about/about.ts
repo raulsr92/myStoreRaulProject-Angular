e22f27ff5e6f3cd2a76a217a2cef6155e3cd7fe7
Raul Sanchez R. <raul.sr92@gmail.com>
2026-05-12 12:08:44 -0500

    Clase 55 - Manipulacion del DOM con Directivas de Angular

--- src/app/domains/info/pages/about/about.ts
+++ src/app/domains/info/pages/about/about.ts
@@ -1,10 +1,11 @@
 import { Component, signal } from '@angular/core';
-import { Counter } from '../../../shared/components/counter/counter';
 import { WaveAudio } from '../../components/wave-audio/wave-audio';
+import { Highlight } from '@shared/directives/highlight';
+import { Counter } from '@shared/components/counter/counter';
 
 @Component({
   selector: 'app-about',
-  imports: [Counter, WaveAudio],
+  imports: [Counter, WaveAudio, Highlight],
   templateUrl: './about.html',
   styleUrl: './about.css',
 })
---
+++ src/app/domains/shared/directives/highlight.ts
@@ -0,0 +1,18 @@
+import { Directive, ElementRef, inject } from '@angular/core';
+
+@Directive({
+  selector: '[highlight]',
+})
+export class Highlight {
+
+  element = inject(ElementRef)
+
+  constructor() { }
+
+  ngOnInit(){
+
+    this.element.nativeElement.style.backgroundColor = '#FFEA6C'
+
+  }
+
+}
