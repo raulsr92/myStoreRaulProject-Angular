e4ece9722ce3cfe31fdd33297ec86793935c31ea
Raul Sanchez R. <raul.sr92@gmail.com>
2026-05-14 11:40:18 -0500

    Clase 57 - Ruta comodin

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,6 +3,7 @@ import { Routes } from '@angular/router';
 import { List } from './domains/products/pages/list/list';
 import { About } from './domains/info/pages/about/about';
 import { WaveAudio } from './domains/info/components/wave-audio/wave-audio';
+import { NotFound } from './domains/info/pages/not-found/not-found';
 
 export const routes: Routes = [
   {
@@ -13,5 +14,8 @@ export const routes: Routes = [
     path: 'about',
     component: About
   },
-
+  {
+    path: '**',
+    component: NotFound
+  },
 ];
