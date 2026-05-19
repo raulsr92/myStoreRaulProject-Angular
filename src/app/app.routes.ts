e64797092b523f254078ba4754f481f16be78146
Raul Sanchez R. <raul.sr92@gmail.com>
2026-05-19 07:43:36 -0500

    Clase 59 Nested routes with child routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,15 +4,22 @@ import { List } from './domains/products/pages/list/list';
 import { About } from './domains/info/pages/about/about';
 import { WaveAudio } from './domains/info/components/wave-audio/wave-audio';
 import { NotFound } from './domains/info/pages/not-found/not-found';
+import { Layout } from '@shared/components/layout/layout';
 
 export const routes: Routes = [
   {
     path: '',  //página inicial
-    component: List
-  },
-  {
-    path: 'about',
-    component: About
+    component: Layout,
+    children:[
+      {
+        path: '',  //página inicial
+        component: List
+      },
+      {
+        path: 'about',
+        component: About
+      },
+    ]
   },
   {
     path: '**',
